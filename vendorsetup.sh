ceaf29761b9c698982d64a6727c7c1e6435f7e67
Yograt <fedrix56@gmail.com>
2025-10-25 13:49:17 +0000

    vendorsetup: specified which branch
    Signed-off-by: Yograt <fedrix56@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 git clone -b fifteen-qpr2 https://github.com/yograts-playground/vendor_xiaomi_fog vendor/xiaomi/fog --depth 1
 
 # Clone prebuilt kernel
-git clone https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel --depth 1
+git clone https://github.com/alternoegraha/device_xiaomi_fog-kernel -b ksu-and-bpf device/xiaomi/fog-kernel --depth 1
 
 # Clone LineageOS' hardware/xiaomi
 git clone -b lineage-23.0 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi --depth 1
